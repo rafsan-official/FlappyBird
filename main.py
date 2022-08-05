bf59b00837166dbfc15a9a424a722a188257225c
rafsan <rafsanoff@gmail.com>
2022-08-05 20:09:23 +0600

    Update commit

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def base_movement(window, base_img, var_x): #animated base
     window.blit(base_img, (var_x+336, 640 - 112)) #here 336 = base image's width
 
 
+def bird_momvent(window, bird_img, bird_rect):
+    window.blit(bird_img,bird_rect)
+
+
 def game_build(): #method create
 
     pygame.init() #initialize pygame
@@ -16,6 +20,12 @@ def game_build(): #method create
 
     base_img = pygame.image.load("gallery\\images\\base.png") #base image
     var_x= 0
+
+
+    # bird
+    bird_img = pygame.image.load("gallery\\images\\bird.png")
+    bird_rect = bird_img.get_rect(center = (336/2,640/2))
+
 
     #main loop
     clock = pygame.time.Clock() #control frame rate of the screen
@@ -37,6 +47,14 @@ def game_build(): #method create
 
         if var_x <= -336: #when base image is gone upto it's total width 336
             var_x= 0 #then base image animation will start from begin
+
+
+
+        #bird movment
+        bird_momvent(window, bird_img, bird_rect)
+
+
+
 
 
         clock.tick(60) #frame rate... if i take low then it will lag, if i take high value, then it will run faster
